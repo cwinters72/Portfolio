78479d7ad5e8eba5047958aaf7c90795cee501ed
cwinters72 <32858263+cwinters72@users.noreply.github.com>
2019-10-07 00:24:11 -0500

    Init

---
+++ js/script.js
@@ -0,0 +1,58 @@
+$(document).ready(function() {
+    $('#slides').superslides({
+        animation: "fade",
+        play: 5000,
+        pagination: false
+    });
+    var typed = new Typed(".typed", {
+        strings: ["Student.", "Software Engineer.", "Web Developer."],
+        typeSpeed: 70,
+        loop: true,
+        startDelat: 1000,
+        showCursor: false
+    });
+
+    $(".owl-carousel").owlCarousel( {
+        loop: true,
+        items: 4,
+        responsive: {
+            0: {
+                items: 1
+            },
+            480: {
+                items: 2
+            },
+            768: {
+                items: 3
+            },
+            938: {
+                items: 4
+            }
+        }
+    });
+
+    
+
+    var skillsTopOffset = $(".skillsSection").offset().top;
+    
+    $(window).scroll(function() {
+        if(window.pageYOffset > skillsTopOffset - $(window).height() + 200) {
+            $('.chart').easyPieChart({
+                easing: "easeInOout",
+                barColor: "#FFF",
+                trackColor: false,
+                scaleColor: false,
+                lineWidth: 4,
+                size: 152,
+                onStep: function(from, to, percent) {
+                    $(this.el).find(".percent").text(Math.round(percent));
+                }
+            });
+        }
+    });
+});
+
+
+
+
+
